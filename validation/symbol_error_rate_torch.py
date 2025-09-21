6685bf92a6127c9cdb39f3d7e6f1beee02e3c316
aicelen <enno.aichele@gmail.com>
2025-09-21 10:06:59 +0200

    QUInt8 transformer_encoder

--- validation/symbol_error_rate_torch.py
+++ validation/symbol_error_rate_torch.py
@@ -20,7 +20,7 @@ def calc_symbol_error_rate_for_list(
     if onnx:
         from homr.transformer.staff2score import Staff2Score as Staff2ScoreOnnx
 
-        model = Staff2ScoreOnnx(False)
+        model = Staff2ScoreOnnx()
         result_file = "onnx_ser.txt"
 
     else:
