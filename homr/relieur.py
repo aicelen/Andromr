01d1fc78a6af764b8a0418cd21f64161e55d04ae
aicelen <enno.aichele@gmail.com>
2026-05-06 21:00:41 +0200

    Rewrite relieur to fix a bug (LLM generated)

--- homr/relieur.py
+++ homr/relieur.py
@@ -1,49 +1,102 @@
+import copy
 import os
-from pathlib import Path
+import xml.etree.ElementTree as ET
 from glob import glob
-
-import musicxml.xmlelement.xmlelement as mxl  # type: ignore
-from musicxml.parser.parser import parse_musicxml  # type: ignore
+from pathlib import Path
 
 
-def clef_attributes(clef) -> dict[str]:  # type ignore
-    # take mxl.XMLClef
+def clef_attributes(clef) -> dict[str, str]:
     attributes = {}
-    for a in clef.get_children_of_type(mxl.XMLSign):
-        attributes["Sign"] = a.value_
-    for a in clef.get_children_of_type(mxl.XMLSign):
-        attributes["Line"] = a.value_
-    for a in clef.get_children_of_type(
-        mxl.XMLClefOctaveChange,
-    ):
-        attributes["ClefOctaveChange"] = a.value_
+    sign = clef.find("sign")
+    line = clef.find("line")
+    octave = clef.find("clef-octave-change")
+    if sign is not None and sign.text:
+        attributes["Sign"] = sign.text
+    if line is not None and line.text:
+        attributes["Line"] = line.text
+    if octave is not None and octave.text:
+        attributes["ClefOctaveChange"] = octave.text
     return attributes
 
 
-def time_attributes(time) -> dict[str]:  # type ignore
-    # take mxl.XMLTime
+def time_attributes(time) -> dict[str, str]:
     attributes = {}
-    for a in time.get_children_of_type(mxl.XMLBeats):
-        attributes["Beats"] = a.value_
-    for a in time.get_children_of_type(mxl.XMLBeatType):
-        attributes["BeatType"] = a.value_
-    for a in time.get_children_of_type(mxl.XMLInterchangeable):
-        attributes["Interchangeable"] = a.value_
-    for a in time.get_children_of_type(mxl.XMLSenzaMisura):
-        attributes["SenzaMisura"] = a.value_
+    beats = time.find("beats")
+    beat_type = time.find("beat-type")
+    interchangeable = time.find("interchangeable")
+    senza_misura = time.find("senza-misura")
+    if beats is not None and beats.text:
+        attributes["Beats"] = beats.text
+    if beat_type is not None and beat_type.text:
+        attributes["BeatType"] = beat_type.text
+    if interchangeable is not None and interchangeable.text:
+        attributes["Interchangeable"] = interchangeable.text
+    if senza_misura is not None and senza_misura.text:
+        attributes["SenzaMisura"] = senza_misura.text
     return attributes
 
 
-def key_attributes(key) -> dict[str]:  # type ignore
-    # take mxl.XMLKey
+def key_attributes(key) -> dict[str, str]:
     attributes = {}
-    for a in key.get_children_of_type(mxl.XMLFifths):
-        attributes["Fifths"] = a.value_
-    for a in key.get_children_of_type(mxl.XMLKeyAlter):
-        attributes["KeyAlter"] = a.value_
-    for a in key.get_children_of_type(mxl.XMLMode):
-        attributes["Mode"] = a.value_
+    fifths = key.find("fifths")
+    key_alter = key.find("key-alter")
+    mode = key.find("mode")
+    if fifths is not None and fifths.text:
+        attributes["Fifths"] = fifths.text
+    if key_alter is not None and key_alter.text:
+        attributes["KeyAlter"] = key_alter.text
+    if mode is not None and mode.text:
+        attributes["Mode"] = mode.text
     return attributes
+
+
+def get_part_attributes(part) -> dict[str, object]:
+    attributes = {}
+    for measure in part.findall("measure"):
+        for attrib in measure.findall("attributes"):
+            div = attrib.find("divisions")
+            key = attrib.find("key")
+            xtime = attrib.find("time")
+            clef = attrib.find("clef")
+            if div is not None and div.text:
+                attributes["Divisions"] = div.text
+            if key is not None:
+                attributes["Key"] = key_attributes(key)
+            if xtime is not None:
+                attributes["Time"] = time_attributes(xtime)
+            if clef is not None:
+                attributes["Clef"] = clef_attributes(clef)
+    return attributes
+
+
+def preprocess_measure(measure, last_attributes: dict[str, object], debug: bool = False) -> None:
+    for attrib in measure.findall("attributes"):
+        div = attrib.find("divisions")
+        if div is not None and last_attributes.get("Divisions") == div.text:
+            attrib.remove(div)
+            if debug:
+                print(f"Remove division at measure {measure.get('number')}")
+
+        key = attrib.find("key")
+        if key is not None and last_attributes.get("Key") == key_attributes(key):
+            attrib.remove(key)
+            if debug:
+                print(f"Remove key at measure {measure.get('number')}")
+
+        xtime = attrib.find("time")
+        if xtime is not None and last_attributes.get("Time") == time_attributes(xtime):
+            attrib.remove(xtime)
+            if debug:
+                print(f"Remove time at measure {measure.get('number')}")
+
+        clef = attrib.find("clef")
+        if clef is not None and last_attributes.get("Clef") == clef_attributes(clef):
+            attrib.remove(clef)
+            if debug:
+                print(f"Remove clef at measure {measure.get('number')}")
+
+        if len(attrib) == 0:
+            measure.remove(attrib)
 
 
 def merge_xmls(
@@ -54,88 +107,51 @@ def merge_xmls(
     sorted_list = get_file_list(concat, debug=debug)
     if not sorted_list:
         return None, 0, 0
-    # Main file is the first of the list
+
     main_file = sorted_list[0]
     print(f"Starting with {main_file}")
-    m = parse_musicxml(main_file)
-    # look for the last key and last divisions
-    last_parts_attributes = []
-    part1 = None  # Initialize part1 to avoid UnboundLocalError
-    for part1 in m.get_children_of_type(mxl.XMLPart):
-        part_attributes = {}
-        for measure in part1.get_children_of_type(mxl.XMLMeasure):
-            for attrib in measure.get_children_of_type(mxl.XMLAttributes):
-                for div in attrib.get_children_of_type(mxl.XMLDivisions):
-                    part_attributes["Divisions"] = div.value_
-                for key in attrib.get_children_of_type(mxl.XMLKey):
-                    part_attributes["Key"] = key_attributes(key)
-                for xtime in attrib.get_children_of_type(mxl.XMLTime):
-                    part_attributes["Time"] = time_attributes(xtime)
-                for clef in attrib.get_children_of_type(mxl.XMLClef):
-                    part_attributes["Clef"] = clef_attributes(clef)
-        last_parts_attributes.append(part_attributes)
+    tree = ET.parse(main_file)
+    root = tree.getroot()
+    main_parts = root.findall("part")
+    last_parts_attributes = [get_part_attributes(part) for part in main_parts]
+
     for f in sorted_list[1:]:
-        # new file to add
         if debug:
             print(f"Processing {f}")
-        b = parse_musicxml(f)
-        ip = 0
-        for part1 in m.get_children_of_type(mxl.XMLPart):
-            # each part from the main score
-            current_len = len(part1.get_children_of_type(mxl.XMLMeasure))
+        new_root = ET.parse(f).getroot()
+
+        for i, part1 in enumerate(main_parts):
+            current_len = len(part1.findall("measure"))
             if debug:
                 print(f"Main part has {current_len} measures")
-            ib = 0
-            for part in b.get_children_of_type(mxl.XMLPart):
-                if ib == ip:
-                    # we add the part of the new file having the same order as the part from the main score, else we pass
-                    for measure in part.get_children_of_type(mxl.XMLMeasure):
-                        new_number = str(int(measure.number) + current_len)
-                        if int(measure.number) == 1:
-                            for attrib in measure.get_children_of_type(mxl.XMLAttributes):
-                                for div in attrib.get_children_of_type(mxl.XMLDivisions):
-                                    if last_parts_attributes[ib]["Divisions"] == div.value_:
-                                        attrib.remove(div)
-                                        if debug:
-                                            print(
-                                                f"Remove division at measure {new_number}, part {ib + 1}"
-                                            )
-                                for key in attrib.get_children_of_type(mxl.XMLKey):
-                                    if last_parts_attributes[ib]["Key"] == key_attributes(key):
-                                        if debug:
-                                            print(
-                                                f"Remove key at measure {new_number}, part {ib + 1}"
-                                            )
-                                        attrib.remove(key)
-                                for xtime in attrib.get_children_of_type(mxl.XMLTime):
-                                    if last_parts_attributes[ib]["Time"] == time_attributes(xtime):
-                                        attrib.remove(xtime)
-                                        if debug:
-                                            print(
-                                                f"Remove time at measure {new_number}, part {ib + 1}"
-                                            )
-                                for clef in attrib.get_children_of_type(mxl.XMLClef):
-                                    if last_parts_attributes[ib]["Clef"] == clef_attributes(clef):
-                                        attrib.remove(clef)
-                                        if debug:
-                                            print(
-                                                f"Remove clef at measure {new_number}, part {ib + 1}"
-                                            )
-                        measure.number = new_number
-                        part1.add_child(measure)
-                        if debug:
-                            print(f"Added measure {new_number}, part {ib + 1}")
-                    current_len = len(part1.get_children_of_type(mxl.XMLMeasure))
-                ib += 1
-            ip += 1
-    m.write(path_out)
+
+            new_parts = new_root.findall("part")
+            if i >= len(new_parts):
+                continue
+
+            for measure in new_parts[i].findall("measure"):
+                new_measure = copy.deepcopy(measure)
+                new_number = str(int(measure.get("number")) + current_len)
+                new_measure.set("number", new_number)
+
+                if measure.get("number") == "1":
+                    preprocess_measure(new_measure, last_parts_attributes[i], debug=debug)
+
+                part1.append(new_measure)
+                if debug:
+                    print(f"Added measure {new_number}, part {i + 1}")
+
+            last_parts_attributes[i] = get_part_attributes(part1)
+
+    ET.indent(tree, space="  ")
+    tree.write(path_out, encoding="utf-8", xml_declaration=True)
+    return None, 0, 0
 
 
 def get_file_list(
     concat: tuple[str],
     debug=False,
 ) -> list[str]:
-    # get the list of files
     sorted_list = []
 
     for pattern in concat:
@@ -166,5 +182,5 @@ def get_file_list(
 
 if __name__ == "__main__":
     merge_xmls(
-        ["images/parrots/p0.musicxml", "images/parrots/p1.musicxml"], "parrots_merged.musicxml"
+        ["test_relieur.musicxml", "test_relieur.musicxml"], "test_relieur_merged.musicxml"
     )
